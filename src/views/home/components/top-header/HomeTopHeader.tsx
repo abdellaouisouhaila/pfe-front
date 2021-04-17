85bf84a205b602fea80b72e0cf78652d5a2e10c6
mohamed <benhassen@brainstack.net>
2021-04-17 12:37:01 +0200

    small fix

--- src/views/home/components/top-header/HomeTopHeader.tsx
+++ src/views/home/components/top-header/HomeTopHeader.tsx
@@ -9,14 +9,20 @@ export const HomeTopHeader = () => {
       <Col push={2} span={16} className="col">
         <h3>Livraison gratuite pour les commandes de plus de 199 TL!</h3>
       </Col>
-      <Col span={6} className="col last"  >
+      <Col span={6} className="col last">
         <h3>
           <Space size={20}>
             Kargom Nerede?
-            <FacebookFilled />
+            <a rel="noreferrer" target="_blank" href="https://www.facebook.com/">
+              <FacebookFilled />
+            </a>
             <Divider className="divider" type="vertical" />
-            <TwitterSquareFilled />
-            <InstagramFilled />
+            <a rel="noreferrer" target="_blank" href="https://www.twitter.com/">
+              <TwitterSquareFilled />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/" rel="noreferrer">
+              <InstagramFilled />
+            </a>
           </Space>
         </h3>
       </Col>
