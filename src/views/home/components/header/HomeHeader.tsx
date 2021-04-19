7660e712a4dfd5338e5c78178e7682f1a0b92a60
mohamed <benhassen@brainstack.net>
2021-04-19 19:26:15 +0200

    small fix

--- src/views/home/components/header/HomeHeader.tsx
+++ src/views/home/components/header/HomeHeader.tsx
@@ -2,16 +2,14 @@ import { Col, Row, Space, Input } from "antd";
 import React from "react";
 import "./HomeHeader.css";
 import {
-  SearchOutlined,
   UserOutlined,
   UserAddOutlined,
-  RightSquareFilled,
 } from "@ant-design/icons";
 const { Search } = Input;
 
 export const HomeHeader = () => {
   return (
-    <Row className="header" gutter={20}>
+    <Row className="header" gutter={16}>
       <Col span={8} className="col_logo">
         <img
           className="header_logo"
@@ -19,16 +17,10 @@ export const HomeHeader = () => {
           src="https://www.veganbakkal.com.tr/Uploads/EditorUploads/logo-veganbakkal.png"
         />
       </Col>
-      <Col span={10} className="col_search">
+      <Col span={10} className="col_search" >
         <Search
           placeholder="Search"
-          enterButton={
-            <RightSquareFilled
-              style={{
-                fontSize: "22px",
-              }}
-            />
-          }
+          enterButton
           size="large"
           loading={false}
         />
