5e92e7486e087bad74a98101bce9bc1acc7edd73
mohamed <benhassen@brainstack.net>
2021-04-19 19:11:00 +0200

    small fix

--- src/views/home/components/header/HomeHeader.tsx
+++ src/views/home/components/header/HomeHeader.tsx
@@ -1,42 +1,49 @@
-import { Col, Row, Space } from "antd";
+import { Col, Row, Space, Input } from "antd";
 import React from "react";
 import "./HomeHeader.css";
-import { SearchOutlined, UserOutlined, UserAddOutlined  } from "@ant-design/icons";
+import {
+  SearchOutlined,
+  UserOutlined,
+  UserAddOutlined,
+  RightSquareFilled,
+} from "@ant-design/icons";
+const { Search } = Input;
 
 export const HomeHeader = () => {
   return (
-    <Row className="header">
+    <Row className="header" gutter={20}>
       <Col span={8} className="col_logo">
-        <img className="header_logo" alt="logo" src="" />
+        <img
+          className="header_logo"
+          alt="logo"
+          src="https://www.veganbakkal.com.tr/Uploads/EditorUploads/logo-veganbakkal.png"
+        />
       </Col>
-      <Col span={8} className="col_search">
-        <input type="text" className="header_searchInput" placeholder="Rechercher" />
-        <SearchOutlined className="header_searchIcon"  />
-    
+      <Col span={10} className="col_search">
+        <Search
+          placeholder="Search"
+          enterButton={
+            <RightSquareFilled
+              style={{
+                fontSize: "22px",
+              }}
+            />
+          }
+          size="large"
+          loading={false}
+        />
       </Col>
-   
-      <Col span={8} className="col_sign">
+      <Col push={2} span={4} className="col-auth">
         <h3>
           <Space size={20}>
             <UserOutlined />
             <span>IDENTIFIANT</span>
-     
+
             <UserAddOutlined />
             <span>S'INSCRIRE</span>
           </Space>
         </h3>
-      </Col> 
-      <Col span={4} className="col_category">
-        <h3>
-        
-          tous les produits
-        
-        
-
-        </h3>
-
-
       </Col>
-    </Row>  
+    </Row>
   );
 };
