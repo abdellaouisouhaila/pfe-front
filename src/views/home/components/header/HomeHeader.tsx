3b469c77998aec275cc11a3b572ee82abd7c1f2f
mohamed <benhassen@brainstack.net>
2021-04-21 01:32:05 +0200

    added menu

--- src/views/home/components/header/HomeHeader.tsx
+++ src/views/home/components/header/HomeHeader.tsx
@@ -1,10 +1,7 @@
-import { Col, Row, Space, Input } from "antd";
+import { Col, Row, Space, Input, Menu } from "antd";
 import React from "react";
 import "./HomeHeader.css";
-import {
-  UserOutlined,
-  UserAddOutlined,
-} from "@ant-design/icons";
+import { UserOutlined, UserAddOutlined } from "@ant-design/icons";
 const { Search } = Input;
 
 export const HomeHeader = () => {
@@ -17,13 +14,8 @@ export const HomeHeader = () => {
           src="https://www.veganbakkal.com.tr/Uploads/EditorUploads/logo-veganbakkal.png"
         />
       </Col>
-      <Col span={10} className="col_search" >
-        <Search
-          placeholder="Search"
-          enterButton
-          size="large"
-          loading={false}
-        />
+      <Col span={10} className="col_search">
+        <Search placeholder="Search" enterButton size="large" loading={false} />
       </Col>
       <Col push={2} span={4} className="col-auth">
         <h3>
@@ -36,6 +28,34 @@ export const HomeHeader = () => {
             </Space>
           </span>
         </h3>
+      </Col>
+      <Col
+        span={24}
+        style={{
+          marginTop: "5vh",
+        }}
+      >
+        <Menu
+          style={{
+            fontSize: "22px",
+            width:"100%",
+            border:"none",
+            background:"#f5f5f5",
+            height:"6vh",
+            borderRadius:"1em",
+            padding:"6px"
+            
+          }}
+          mode="horizontal"
+        >
+          <Menu.Item key="TÜM ÜRÜNLER">TÜM ÜRÜNLER</Menu.Item>
+          <Menu.Item key="Menu1">Menu1</Menu.Item>
+          <Menu.Item key="Menu2">Menu2</Menu.Item>
+          <Menu.Item key="Menu3">Menu3</Menu.Item>
+          <Menu.Item key="Menu4">Menu4</Menu.Item>
+          <Menu.Item key="Menu5">Menu5</Menu.Item>
+          <Menu.Item key="Menu6">Menu6</Menu.Item>
+        </Menu>
       </Col>
     </Row>
   );
